a439a8f2c5807d83be5af292095e67be72eda2a6
Samuel Ayodeji <ayodejisoyebo@gmail.com>
2024-06-29 22:09:55 +0100

    responsive navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,98 @@
+import { useCallback, useEffect, useState } from "react";
+
 function Header() {
+  const [buttonDisplay, setButtonDisplay] = useState(false);
+  const [harmburgerDisplay, setHarmburgerDisplay] = useState(false);
+  const [displayPopUp, setDisplayPopUp] = useState(false);
+
+  const handleResize = () => {
+    if (window.innerWidth > 992) {
+      setButtonDisplay(true);
+      setHarmburgerDisplay(false);
+    } else {
+      setButtonDisplay(false);
+      setHarmburgerDisplay(true);
+    }
+  };
+
+  useEffect(() => {
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const toggleDisplayPopUp = useCallback(
+    () => setDisplayPopUp(!displayPopUp),
+    [displayPopUp]
+  );
   return (
-    <div className="px-20 py-3 bg-[#017efe]">
+    <div className="px-7 lg:px-20 py-3 bg-[#017efe] relative">
       <div className="flex justify-between items-center">
-        <h3 className="text-[#fdfdfd] font-semibold text-3xl leading-[38.74px]">
+        <h3 className="text-[#fdfdfd] font-semibold text-2xl lg:text-3xl leading-[38.74px] whitespace-nowrap">
           DEMO Streaming
         </h3>
 
-        <div className="flex items-center gap-8">
-          <button
-            type="button"
-            className="border-none outline-none text-sm text-[#fdfdfd] font-semibold leading-[16.44px]"
-          >
-            Log in
-          </button>
+        {buttonDisplay === true && (
+          <div className="flex items-center gap-8">
+            <button
+              type="button"
+              className="border-none outline-none text-sm text-[#fdfdfd] font-semibold leading-[16.44px]"
+            >
+              Log in
+            </button>
 
+            <button
+              type="button"
+              className="bg-[#424242] px-4 py-3 text-sm text-[#fdfdfd] font-semibold leading-[16.44px]"
+            >
+              Start your free trial
+            </button>
+          </div>
+        )}
+
+        {harmburgerDisplay === true && (
           <button
             type="button"
-            className="bg-[#424242] px-4 py-3 text-sm text-[#fdfdfd] font-semibold leading-[16.44px] bg-[]"
+            className="border-none outline-none"
+            onClick={toggleDisplayPopUp}
           >
-            Start your free trial
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect y="4" width="24" height="2" fill="#fdfdfd" />
+              <rect y="11" width="24" height="2" fill="#fdfdfd" />
+              <rect y="18" width="24" height="2" fill="#fdfdfd" />
+            </svg>
           </button>
-        </div>
+        )}
+
+        {displayPopUp === true && (
+          <div className="absolute right-0 top-[65px] w-3/5 bg-[#fdfdfd] shadow-md rounded-sm p-3">
+            <div className="flex flex-col gap-2">
+              <button
+                type="button"
+                className="text-sm text-[#424242] px-4 py-3 border border-[#424242] font-semibold leading-[16.44px]"
+              >
+                Log in
+              </button>
+
+              <button
+                type="button"
+                className="bg-[#424242] px-4 py-3 text-sm text-[#fdfdfd] font-semibold leading-[16.44px]"
+              >
+                Start your free trial
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
